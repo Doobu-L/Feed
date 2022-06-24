dee5eeee75ea71b6df310065f57ec9c031fb313c
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-06-24 18:24:37 +0900

    MVC 테스트코드 테스트

--- src/test/java/com/mypj/aaa/securityTest/SecurityTest.java
+++ src/test/java/com/mypj/aaa/securityTest/SecurityTest.java
@@ -1,8 +1,34 @@
 package com.mypj.aaa.securityTest;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.mypj.aaa.controller.HelloController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
 public class SecurityTest {
 
+  private MockMvc mvc;
+
+  @Test
+  @DisplayName("Hello Test")
+  void Hello_Test() throws Exception{
+    String hello = "hello";
+
+    mvc.perform(get("/hello"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("Hellowwwwwwww"));
+
+  }
 
 
 }
