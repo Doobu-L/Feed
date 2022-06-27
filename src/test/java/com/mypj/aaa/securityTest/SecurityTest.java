261e9ba17cefd1450d7649160c09b7116190e45a
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-06-27 17:24:05 +0900

    Security Test 연습

--- src/test/java/com/mypj/aaa/securityTest/SecurityTest.java
+++ src/test/java/com/mypj/aaa/securityTest/SecurityTest.java
@@ -2,33 +2,70 @@ package com.mypj.aaa.securityTest;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.mypj.aaa.controller.HelloController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mypj.aaa.repository.UserRepository;
+import com.mypj.aaa.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@WebMvcTest(HelloController.class)
+@ExtendWith(MockitoExtension.class)
 public class SecurityTest {
 
-  private MockMvc mvc;
+  MockMvc mockMvc;
 
-  @Test
-  @DisplayName("Hello Test")
-  void Hello_Test() throws Exception{
-    String hello = "hello";
+  @Autowired
+  WebApplicationContext context;
+  ObjectMapper objectMapper;
 
-    mvc.perform(get("/hello"))
-        .andExpect(status().isOk())
-        .andExpect(content().string("Hellowwwwwwww"));
+  @Mock
+  UserService userService;
+  @Mock
+  UserRepository userRepository;
+  @Mock
+  PasswordEncoder passwordEncoder ;
 
+  @BeforeEach
+  public void setup(){
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    this.objectMapper = new ObjectMapper();
+    this.userService = new UserService(userRepository,passwordEncoder);
   }
 
+  @Test
+  @WithMockUser(username = "test",roles = "USER")
+  void getMyUserInfo() throws Exception {
+    mockMvc.perform(get("/api/users"))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithMockUser(username = "test",roles = "ADMIN")
+  void getUserINfo() throws Exception {
+    mockMvc.perform(get("/api/users/1"))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
 
 }
