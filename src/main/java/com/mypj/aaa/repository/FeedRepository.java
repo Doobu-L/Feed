e81a1e8cc2885a9751271450efc0ffd7090023a8
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-07-04 11:47:05 +0900

    Feed Create , Read

---
+++ src/main/java/com/mypj/aaa/repository/FeedRepository.java
@@ -0,0 +1,8 @@
+package com.mypj.aaa.repository;
+
+import com.mypj.aaa.domain.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedRepository extends JpaRepository<Feed,Long> {
+
+}
---
+++ src/main/java/com/mypj/aaa/service/FeedService.java
@@ -0,0 +1,37 @@
+package com.mypj.aaa.service;
+
+import com.mypj.aaa.domain.dto.FeedDto;
+import com.mypj.aaa.domain.entity.Feed;
+import com.mypj.aaa.domain.entity.User;
+import com.mypj.aaa.repository.FeedRepository;
+import com.mypj.aaa.repository.UserRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+
+  private final FeedRepository feedRepository;
+  private final UserRepository userRepository;
+
+  public List<FeedDto> getAllFeedList(){
+    return feedRepository.findAll().stream().map(FeedDto::new).collect(Collectors.toList());
+  }
+
+  @Transactional
+  public FeedDto getFeed(long id){
+    Feed feed = feedRepository.findById(id).orElseThrow(()->new RuntimeException("존재하지 않는 게시글입니다."));
+    return new FeedDto(feed);
+  }
+
+  @Transactional
+  public void newFeed(FeedDto feedDto){
+    User user =  userRepository.findOneWithAuthoritiesByUserId(feedDto.getUserId()).orElseThrow(()->new RuntimeException("존재하지않는 유저입니다."));
+    Feed feed = Feed.builder().title(feedDto.getTitle()).content(feedDto.getContent()).user(user).build();
+    feedRepository.save(feed);
+  }
+}
