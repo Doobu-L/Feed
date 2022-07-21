91aa07b188a9e524642b513d66be1067655ec0a5
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-07-21 18:49:57 +0900

    view에 넘겨주기위한 컬럼 추가

--- src/main/java/com/mypj/aaa/domain/dto/ScheduleDto.java
+++ src/main/java/com/mypj/aaa/domain/dto/ScheduleDto.java
@@ -3,6 +3,7 @@ package com.mypj.aaa.domain.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mypj.aaa.domain.entity.Schedule;
 import io.swagger.v3.oas.annotations.Hidden;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.ToString;
@@ -19,9 +20,20 @@ public class ScheduleDto {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime targetDateTime;
 
+  private int month;
+  private int day;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "Asia/Seoul")
+  private int hour;
+  private int minute;
+
   public ScheduleDto(Schedule schedule){
+    this.id = id;
     this.title = schedule.getTitle();
     this.targetDateTime = schedule.getTargetDateTime();
+    this.month = this.targetDateTime.getMonthValue();
+    this.day = this.targetDateTime.getDayOfMonth();
+    this.hour = this.targetDateTime.getHour();
+    this.minute = this.targetDateTime.getMinute();
   }
 
 }
